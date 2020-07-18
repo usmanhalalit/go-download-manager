428942f1a482c27d5f0f6de64e5d75746685e4d2
Muhammad Usman <hi@usman.it>
2020-07-18 16:21:05 +0100

    Boom ... :bomb:

---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Download struct {
+	Url           string
+	TargetPath    string
+	TotalSections int
+}
+
+func main() {
+	startTime := time.Now()
+	d := Download{
+		// Provide the URL tod download,
+		//	example: https://www.dropbox.com/s/lgvhj/sample.mp4?dl=1
+		Url: "",
+		// Provide the target file path with extension, example: sample.mp4
+		TargetPath: "",
+		// Number of sections/connections to make to the server
+		TotalSections: 10,
+	}
+	err := d.Do()
+	if err != nil {
+		log.Printf("An error occured while downloading the file: %s\n", err)
+	}
+	fmt.Printf("Download completed in %v seconds\n", time.Now().Sub(startTime).Seconds())
+}
+
+// Start the download
+func (d Download) Do() error {
+	fmt.Println("Checking URL")
+	r, err := d.getNewRequest("HEAD")
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Got %v\n", resp.StatusCode)
+
+	if resp.StatusCode > 299 {
+		return errors.New(fmt.Sprintf("Can't process, response is %v", resp.StatusCode))
+	}
+
+	size, err := strconv.Atoi(resp.Header.Get("Content-Length"))
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Size is %v bytes\n", size)
+
+	var sections = make([][2]int, d.TotalSections)
+	eachSize := size / d.TotalSections
+	fmt.Printf("Each size is %v bytes\n", eachSize)
+
+	// example: if file size is 100 bytes, our section should like:
+	// [[0 10] [11 21] [22 32] [33 43] [44 54] [55 65] [66 76] [77 87] [88 98] [99 99]]
+	for i := range sections {
+		if i == 0 {
+			// starting byte of first section
+			sections[i][0] = 0
+		} else {
+			// starting byte of other sections
+			sections[i][0] = sections[i-1][1] + 1
+		}
+
+		if i < d.TotalSections-1 {
+			// ending byte of other sections
+			sections[i][1] = sections[i][0] + eachSize
+		} else {
+			// ending byte of other sections
+			sections[i][1] = size - 1
+		}
+	}
+
+	log.Println(sections)
+	var wg sync.WaitGroup
+	// download each section concurrently
+	for i, s := range sections {
+		wg.Add(1)
+		go func(i int, s [2]int) {
+			defer wg.Done()
+			err = d.downloadSection(i, s)
+			if err != nil {
+				panic(err)
+			}
+		}(i, s)
+	}
+	wg.Wait()
+
+	return d.mergeFiles(sections)
+}
+
+// Download a single section and save content to a tmp file
+func (d Download) downloadSection(i int, c [2]int) error {
+	r, err := d.getNewRequest("GET")
+	if err != nil {
+		return err
+	}
+	r.Header.Set("Range", fmt.Sprintf("bytes=%v-%v", c[0], c[1]))
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode > 299 {
+		return errors.New(fmt.Sprintf("Can't process, response is %v", resp.StatusCode))
+	}
+	fmt.Printf("Downloaded %v bytes for section %v\n", resp.Header.Get("Content-Length"), i)
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(fmt.Sprintf("section-%v.tmp", i), b, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get a new http request
+func (d Download) getNewRequest(method string) (*http.Request, error) {
+	r, err := http.NewRequest(
+		method,
+		d.Url,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	r.Header.Set("User-Agent", "Silly Download Manager v001")
+	return r, nil
+}
+
+// Merge tmp files to single file and delete tmp files
+func (d Download) mergeFiles(sections [][2]int) error {
+	f, err := os.OpenFile(d.TargetPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	for i := range sections {
+		tmpFileName := fmt.Sprintf("section-%v.tmp", i)
+		b, err := ioutil.ReadFile(tmpFileName)
+		if err != nil {
+			return err
+		}
+		n, err := f.Write(b)
+		if err != nil {
+			return err
+		}
+		err = os.Remove(tmpFileName)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%v bytes merged\n", n)
+	}
+	return nil
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Golang Download Manager - Weekend Project
+
+A concurrent download manager written in pure Go without any dependency.
+
+I wrote this code for my YouTube video:
+
+<a href="http://www.youtube.com/watch?v=vdhSk8vCx-k
+" target="_blank"><img src="https://www.dropbox.com/s/r2eamsbai84wv24/Go%20Download%20Manager.png?raw=1" 
+alt="Watch on YouTube" width="240" height="180" border="0" /></a>
+
+**Watch video to understand the usage and every line of the code.**
+
+## Usage
+
+Provide the file URL and download destination in the main func and run:
+```
+go run main.go
+```
+
+___
+Follow me on [Twitter](https://twitter.com/halalit_usman/).
+
+ 
